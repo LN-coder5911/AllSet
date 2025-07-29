fba8095892215578c924187066990a3eebda238b
Alexandra Akyuz <bananalex2009@gmail.com>
2025-07-29 11:46:08 -0700

    added date

--- AllSet/ContentView.swift
+++ AllSet/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State var name = ""
+    let currentDate = Date()
     
     var body: some View {
         NavigationStack{
             ZStack{
                 Color.vanilla
                     .ignoresSafeArea()
-                Text("Hello, \(name)")
+                HStack{
+                    
+                    Text("Hello, \(name)")
+                    Text(currentDate, style: .date)
+                }
                 
                 
             }//Zstack
