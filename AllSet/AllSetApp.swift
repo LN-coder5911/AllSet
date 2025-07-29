5ce36930474a341e6820fe7aaa7680b4f55c4ac7
Lena Nichols <lena.k.nichols@gmail.com>
2025-07-29 09:51:58 -0700

    Initial Commit

---
+++ AllSet/AllSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllSetApp.swift
+//  AllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AllSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
