de30365979d4b28cb08eb2980883f5a7b269e2da
Naina Subherwal <nainasubherwal@gmail.com>
2025-07-29 10:44:19 -0700

    Fix color name

---
+++ AllSet/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  AllSet
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+
+        
+        
+    }//body
+    
+}//struct
+
+#Preview {
+    LoginView()
+}
